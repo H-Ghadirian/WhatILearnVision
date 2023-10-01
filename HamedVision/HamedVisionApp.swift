13a672b752d55e23e02e44b889c16a9e6a2b090b
Hamed Ghadirian <H.qadirian@gmail.com>
2023-10-01 23:48:09 +0200

    hello world

---
+++ HamedVision/HamedVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  HamedVisionApp.swift
+//  HamedVision
+//
+//  Created by Ghadirian, Hamed, HSE DE on 01.10.23.
+//
+
+import SwiftUI
+
+@main
+struct HamedVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
